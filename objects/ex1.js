ed671d0ba384b80f8b111c2adb4847c0222b6759
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-29 12:18:37 -0300

    fineshed

--- objects/ex1.js
+++ objects/ex1.js
@@ -1,5 +1,15 @@
+let nameStudents = {
+    student1: 'Vinícius',
+    student2: 'Marcos',
+    student3: 'João',
+    student4: 'Arthur',
+    student5: 'Ycaro',
+}
+
+let studentsName = ['Vinícius', 'Marcos', 'Ycaro', 'João', 'Arthur']
+console.table(studentsName)
+console.table(nameStudents)
 
 
-
-// Criar uma coleção de nomes de alunos, a coleção deve ter pelo menos 5 nomes. 
+// Criar um array e objects de nomes de alunos, a coleção deve ter pelo menos 5 nomes. 
 // Mostre no console
